360e7c85bb10bd73990937bbea5548f8a6979214
oeyoews <2956398608@qq.com>
2021-07-01 20:57:08 +0800

    add custom

--- sh/archlinuxcn.sh
+++
@@ -1,4 +0,0 @@
-echo "[archlinuxcn]\n Server = https://mirrors.sjtug.sjtu.edu.cn/archlinux-cn/$arch" >> /etc/pacman.conf
-echo success add archlinux-cn
-yes | sudo pacman -S archlinuxcn-keyring
-yes | sudo pacman -S yay
