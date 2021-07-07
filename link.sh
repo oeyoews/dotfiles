5b27d72ee53cf83085a9e62330a6dc380894bdfd
oeyoews <2956398608@qq.com>
2021-07-07 10:40:30 +0800

    tidy link.sh

--- link.sh
+++ link.sh
@@ -1,71 +1,56 @@
 #!/bin/bash
 #########################################################
-
 function Green_Success(){
-
-        echo '=================================================';
-
-        printf '\033[1;32;40m[success]  %b\033[0m\n' "$1";
-
+    echo '=================================================';
+    printf '\033[1;32;40m[success]  %b\033[0m\n' "$1";
 }
-
 function Yellow_Warnning(){
-
-        echo '=================================================';
-
-        printf '\033[1;33;40m[warnning]  %b\033[0m\n' "$1";
-
+    echo '=================================================';
+    printf '\033[1;33;40m[warnning]  %b\033[0m\n' "$1";
 }
-
 function Red_Error(){
-
-        echo '=================================================';
-
-        printf '\033[1;31;40m[error]  %b\033[0m\n' "$1";
-
-        exit 1;
-
+    echo '=================================================';
+    printf '\033[1;31;40m[error]  %b\033[0m\n' "$1";
+    exit 1;
 }
-
-# Green_Success "成功"
-#
-# Yellow_Warnning "警告"
-#
-# Red_Error "错误"
-
+# Green_Success "FINSH"
+# Yellow_Warnning "WARNING"
+# Red_Error "ERROR"
 #########################################################
-echo START:
+echo LINK_START:
 
-# LPATH=$HOME/src/DOTFILES
-# .ZSHRC
-ln -si $PWD/.zshrc ~
-
-# OMZ
 # note if .omz exit it will show .omz/omz need to add a adjust
+MS3="rm omz directory and ln a new omz directory"
+MS$="success link OMZ"
 if [ -d ~/.omz/ ]; then
-rm ~/.omz
-ln -si $PWD/omz ~/.omz && echo "rm omz directory and ln a new omz directory"
-else
-ln -si $PWD/omz ~/.omz  && echo "directly to ln a .omz"
+    rm ~/.omz
+    echo $MS3
 fi
-echo ${SUCCESS} link OMZ
+ln -si $PWD/omz ~/.omz
+echo $MS4
+Green_Success "OMZ"
 
 # VSCODE
 # if vscode not install and this dir will not exit ,so it will make a error
-VSCODE="~/.config/Code/User/"
-if [ ! -d $VSCODE ]; then
-  echo "this vscode may be not installed, please install it!"
-elif [ -e $VSCODE/settings.json ]; then
-  rm $VSCODE/settings.json &&  ln -si $PWD/settings.json $VSCODE/
-  echo "rm settings and creat a new it"
-else
-  ln -si $PWD/settings.json $VSCODE/ && echo  "creat a new settings.json"
+export VSCODE=~/.config/Code/User
+MS1="This vscode may be not installed, please install it!"
+MS2="rm exist file vscode_configure(old)"
+if [ ! -d ${VSCODE} ]; then
+  mkdir -r ${VSCODE}
+  echo $MS1
+elif [ -e ${VSCODE}/settings.json ]; then
+  rm ${VSCODE}/settings.json
+  echo $MS2
 fi
-  # INPUT
+
+ln -si $PWD/settings.json ${VSCODE}/
+# INPUT
 ln -si $PWD/.pam_environment ~
 # GITCONFIG
 ln -si $PWD/.gitconfig ~
-# ranger 
-sudo cp -r $PWD/ranger ~/.config/ 
+# ranger
+sudo cp -r $PWD/ranger ~/.config/
+# zshrc
+ln -si $PWD/.zshrc ~
 
-Green_Success "成功"
+Green_Success "FINSH"
