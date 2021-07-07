dd270892d4dcd701c5f82011631296f847e6d05b
oeyoews <2956398608@qq.com>
2021-07-07 19:26:12 +0800

    add some soft

--- SH/software_install.sh
+++ SH/software_install.sh
@@ -2,12 +2,12 @@
 # 2021年 07月 06日 星期二 23:53:25 CST
 
 function pacman() {
-  SW="vim ranger trash-cli base-devel neofetch lolcat translate-shell ripgrep duf tokei cloc"
+  SW="vim ranger trash-cli base-devel neofetch lolcat translate-shell ripgrep duf tokei cloc netease-cloud-music google-chrome foliate emacs obs-studio typora visual-studio-code-bin libreoffice-fresh"
   sudo pacman -S --noconfirm ${SW}
 }
 
 function misc() {
-  yay fcitx-qt5 fcitx-configtool nvm  virtual-desktop com.qq.tim.spark wechat-uos wudao-dict-git fcitx-sogoupinyin gnome-keyring
+  yay fcitx-qt5 fcitx-configtool nvm  virtual-desktop com.qq.tim.spark wechat-uos wudao-dict-git fcitx-sogoupinyin gnome-keyring xunlei-bin
 }
 
 function pip() {
@@ -17,7 +17,7 @@ function pip() {
 function main() {
   pacman
   # pip
-  yay
+  # yay
 }
 
 main
