4a66fe982ec8bc20fdbe60262ac32b516b87cca2
oeyoews <2956398608@qq.com>
2021-07-08 15:53:38 +0800

    fix install_.vim.sh

--- SH/install_.vim.sh
+++ SH/install_.vim.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/bash
 # 2021-07-08 15:41:09 
 
-function file_jusge() {
-  IP="https://hub.fastgit.org/oeyoews/.vim"
-  WHO="$(whoami)"
+IP="https://hub.fastgit.org/oeyoews/.vim"
+WHO="$(whoami)"
+
+function file_judge() {
   if [ -d ~/.vim ]; then
     mv ~/.vim ~/.vim_bk
   fi
+}
+
+function install_vim() {
   git clone --depth 1 ${IP}
   echo "Finish to intall oeyoews/.vim for ${WHO}."
   echo "Just enjoy it!"
 }
 
+function main() {
+  file_judge
+  install_vim
+}
 
+main
