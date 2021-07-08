2c796ca4075818275aabfffeec2306b370860947
oeyoews <2956398608@qq.com>
2021-07-09 00:48:29 +0800

    fix regular expression

--- link.sh
+++ link.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 # 2021-07-08 14:57:21 
 
-function Green_Success(){
-  printf '\033[1;32;40m[success]==>  %b\033[0m\n' "$1";
-}
-
-function Yellow_Warnning(){
-  printf '\033[1;33;40m[warnning]  %b\033[0m\n' "$1";
-}
-
-function Red_Error(){
-  printf '\033[1;31;40m[error]  %b\033[0m\n' "$1";
-  exit 1;
-}
-
 # ranger
 function misc() {
   sudo cp -ir ${PWD}/ranger ~/.config/
@@ -33,8 +20,10 @@ function link_vscode() {
 
 function link_all_dotfiles_in_home() {
   MS2="Success link all dotfiles in home"
-  # ln -si $PWD/dotfiles/.* ~
-  ln -si $PWD/dotfiles/* ~
+  ln -si $PWD/dotfiles/.[a-zA-Z]* ~ 
+  # : can't use .* in git dir, because it can ln . and .. dir.
+  # :so, must be put them in another dir, to avoid dir like .git and .gitconfig and so on
+  # : can't use * to instead of dot, because it can't to instead it use ==> .[a-zA-Z]*
   echo "$MS2"
 }
 
@@ -59,7 +48,7 @@ function print_date() {
 
 function main(){ # NOTE: Function can't is empty
 
-  # link_all_dotfiles_in_home
+  link_all_dotfiles_in_home
   # link_vscode
   print_date
   # Green_Success demo
