b620fea6ec0678162fd807511141c1398c649609
oeyoews <2956398608@qq.com>
2021-07-14 15:12:01 +0800

    This is a auto push for Dotfiles.

--- SH/crontab.sh
+++ SH/crontab.sh
@@ -1,10 +1,10 @@
 function dotfiles() {
-  cd $HOME/src/DOTFILES && git checkout dev && git add . && git commit -m "This is a auto push for Dotfiles." && git push origin dev
+  cd $HOME/src/DOTFILES && git add . && git commit -m "This is a auto push for Dotfiles." && git push
   echo "Date:$(date) User:$(whoami) success for crontab (Dotfiles)" >> $HOME/src/DOTFILES/Log/crontab_for_Dotfiles.log
 }
 
 function note() {
-  cd $HOME/src/note  && git add . && git commit -m "This is a auto push for note." && git push 
+  cd $HOME/src/note  && git add . && git commit -m "This is a auto push for note." && git push
   echo "Date:$(date) User:$(whoami) success for crontab (Dotfiles)" >> $HOME/src/DOTFILES/Log/crontab_for_Note.log
 }
 
