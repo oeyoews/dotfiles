0c67fb8bf45afdaeda1cf9c581fef91d6c959b6d
oeyoews <2956398608@qq.com>
2021-07-18 23:00:01 +0800

    This is a auto push for repo.

--- dotfiles/.omz/lib/functions.zsh
+++ dotfiles/.omz/lib/functions.zsh
@@ -248,7 +248,7 @@ function pym() {
   python -m http.server
 }
 
-function color() {
+function 255_color() {
 for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " \
   ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
