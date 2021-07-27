829443ca97d259a1c4ab50425966c1c73b4edc77
oeyoews <2956398608@qq.com>
2021-07-27 23:43:26 +0800

    This is a auto push for '/home/oeyoews/dotfiles'.

--- sh/crontab.sh
+++ sh/crontab.sh
@@ -5,37 +5,28 @@
 # Author: @oeyoew
 
 set -e
-# use shellcheck to write better script
 
 function color_echo() {
-  # NOTE: printf not print
   printf "\e[35m %b \e[0m" "$1"
 }
 
 function echo_log() {
-  # NOTE: second can't appear whitespace
-  # cat << EOF......\nEOF
   cat>>log<<EOF
-<<<
+=====
 user: $(whoami)
 date: $(date)
->>>
+=====
 
 EOF
-# echo -e "${color_begin} success $color_off"
 color_echo "success  for '$PWD'\n"
 }
 
 function repo() {
-  # -n 判断是否有值
-  # NOTE string
   if [ -n "$(git status -s)" ]
   then
     (git add . 
     git commit -m "This is a auto push for '$PWD'." 
     git push) &>>"$PWD/log"
-    # in repo's directory, touch log and record
-    # test -f "$PWD/log" ||  { touch "$PWD/log"; echo "touch log in '$PWD'" >> "$PWD/log"; }
   else 
     echo -e "This repo is clean for '$PWD/log' \n"
   fi
