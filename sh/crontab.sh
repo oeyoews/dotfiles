08546ad83fc717059b17f2e37d70a073b4b1825f
oeyoews <2956398608@qq.com>
2021-07-28 00:12:34 +0800

    This is a auto push.

--- sh/crontab.sh
+++ sh/crontab.sh
@@ -6,36 +6,15 @@
 
 set -e
 
-function color_echo() {
-  printf "\e[35m %b \e[0m" "$1"
-}
-
-function echo_log() {
-  cat>>log<<EOF
-=====
-user: $(whoami)
-date: $(date)
-=====
-
-EOF
-color_echo "success  for '$PWD'\n"
-}
-
 function repo() {
-  if [ -n "$(git status -s)" ]
-  then
-    (git add . 
-    git commit -m "This is a auto push for '$PWD'." 
-    git push) &>>"$PWD/log"
-  else 
-    echo -e "This repo is clean for '$PWD/log' \n"
-  fi
-  echo_log
+    git add . 
+    git commit -m "This is a auto push." 
+    git push 
 }
 
-function emacs_pull() {
+function emacs_update() {
  cd ~/.emacs.d/ || exit 
- git pull &>/dev/null
+ git pull 
 }
 
 function main() {
@@ -43,8 +22,8 @@ function main() {
   repo
   cd ~/src/note || exit
   repo
-  emacs_pull
-  echo -e "\n\e[32mTime: $SECONDS\n\e[0m"
+  emacs_update
+  echo "$SECONDS"
   # if
 }
 
