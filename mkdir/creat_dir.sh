9547d62f593ca9c18465a0355304b1dd99a8002d
oeyoews <2956398608@qq.com>
2021-09-13 23:22:00 +0800

    rename mk.sh to creak_dir.sh

---
+++ mkdir/creat_dir.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+create_dir() {
+  cd "$HOME"|| exit 0
+  mkdir .config/coc \
+        jupyter repo doc pdf md org temp vscode wechat
+}
+
+main() {
+  create_dir;
+  echo "Creat some directions for you successfully."
+}
+
+main
