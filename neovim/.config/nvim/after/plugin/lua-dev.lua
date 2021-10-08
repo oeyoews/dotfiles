ec027b4d24c1464b8c199e05a666831014a2b135
oeyoews <2956398608@qq.com>
2021-10-08 11:04:01 +0800

    move neovim to archive folder

--- neovim/.config/nvim/after/plugin/lua-dev.lua
+++
@@ -1,8 +0,0 @@
-local luadev = require("lua-dev").setup({
-  lspconfig = {
-    cmd = {"lua-language-server"}
-  },
-})
-
-local lspconfig = require('lspconfig')
-lspconfig.sumneko_lua.setup(luadev)
--- neovim/.config/nvim/autoload/demo.vim
+++
@@ -1,6 +0,0 @@
-" use call demo#demo()
-function demo#demo(name) abort
-  
-  echohl DiffAdd
-  echo "Hi," a:name
-endfunction
