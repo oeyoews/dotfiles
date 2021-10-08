6ca67da38e69fc83a8c6fd66dc0e4f807f043450
oeyoews <2956398608@qq.com>
2021-10-08 18:18:40 +0800

    add new font for statusline

--- archive/cheat/sheets/todo.md
+++ archive/cheat/sheets/todo.md
@@ -1,10 +1,8 @@
 ## II
-- [ ] learn __computer_network_theory__ in bilibili' fangfangfang
-- [ ] try find some projects for lua, like love framwork
-- [ ] try vue
+try to resolve cmp-emoji legth 
 
 ## I
 - [ ] CNP mooc test
-- [ ] android   to make app 
+- [ ] android to make app 
 - [ ] digital图像作业 10-22 (fspecial) set get function 
 - [ ] rhi homework(matlab gui)
--- omz/.omz/lib/private.zsh
+++ omz/.omz/lib/private.zsh
@@ -1,16 +1,9 @@
 # vim
-alias vito='vim ~/dotfiles/archive/todo/todo.org'
 alias viz='vim $HOME/.zshrc'
-alias vic='vim ~/dotfiles/cheat/conf.yml'
 alias vip='vim ~/.omz/lib/private.zsh'
-alias vig='vim .gitignore'
 
 # path
 alias cdd="cd ~/dotfiles/"
-alias cdv="cd ~/.config/nvim"
-alias cdr="cd ~/Repos"
-alias cdt="cd /tmp"
-alias cdu="cd ~/.config/nvim/ultisnips"
 alias soz='source $HOME/.zshrc && exec zsh'
 
 # system
@@ -20,34 +13,28 @@ alias open='xdg-open'
 alias c='clear'
 alias pas='sudo pacman -S'
 alias yayy="yay -Yc"
-alias yayyy="yes | yay -Scc"
 alias yayr='yay -Rscn'
 alias cp="cp -r"
 
 # on
 alias 'onkvm'='sudo virt-manager'
 alias 'onsshd'='sudo systemctl restart sshd.service && systemctl status sshd.service'
-alias 'onbluetooth'="sudo systemctl start bluetooth.service"
 
 # software
 alias ra='ranger'
 alias dust="dust -r"
 alias rm='trash'
-# use vanilla rm, don't use rmm=rm, it's receive
 alias rmm='\rm -rf'
 alias ms='musicbox'
 alias tlpt='sudo tlp-stat -t'
 alias tlpb='sudo tlp-stat -b'
 
-# Chinese to English
 # arguments: -p -u
 alias tse='crow -t en'
 # English to Chinese
 alias ts='crow -b'
 
-# @internet
 alias cip='curl cip.cc'
-alias ping="ping -c 5"
 alias ipr='ip addr | rg 192'
 
 # @git
@@ -66,16 +53,6 @@ alias kdiff="kitty +kitten diff"
 alias kemoj="kitty +kitten unicode_input"
 alias diffk="kitty +kitten diff"
 
-
-## @translate
-# alias crow="crow -b"
-# chinese to english
-# usage: crow -h
-# crow -p foo(foo)
-# crow -u bar(=>)
-alias tse='crow -t en'
-# english to system language (chinese)
-alias ts='crow -b'
 
 # Misc
 alias che="cheat -e"
@@ -96,24 +73,13 @@ export EDITOR=nvim
 export CHEATCOLORS=true
 export CHEAT_CONFIG_PATH="~/dotfiles/archive/cheat/conf.yml"
 export CHEAT_USE_FZF=true
-export COLORTERM=truecolor
 
 # better man page dependcies: bat
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
-# doom emacs
-# emacs -nw -Q
-# emacs -Q <--color=no>
-# time emacs -q -e kill-emacs
-# time emacs -e kill-emacs
-alias eg="emacs"
-alias et="emacs -nw"
-# it's recursive
 alias vim='nvim'
 alias vi='nvim'
 alias e='nvim'
 alias se='nvim -u NONE'
-alias ext=exit
 
-alias grepd="l | grep '>'"
 alias wego='curl wttr.in'
