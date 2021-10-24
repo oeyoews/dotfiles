0e778b6c634932ef4e28e7de3035ce52406f5b1c
oeyoews <2956398608@qq.com>
2021-10-24 20:18:51 +0800

    🌠 update

--- omz/.omz/themes/p10k/p10k-pure.zsh
+++ omz/.omz/themes/p10k/p10k-pure.zsh
@@ -177,7 +177,7 @@
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=44
   # Custom icon.
-   typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=' '
+   typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='  '
 
   ################################[ prompt_char: prompt symbol ]################################
   # Green prompt symbol if the last command succeeded.
