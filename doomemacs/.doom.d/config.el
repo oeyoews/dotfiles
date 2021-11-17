d92bbd4eb4bc21f03063423b4803ebb616bbb220
oeyoews <2956398608@qq.com>
2021-11-17 22:33:41 +0800

    🦧 update doom emacs

---
+++ doomemacs/.doom.d/config.el
@@ -0,0 +1,105 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Tutorial for doom:
+;; https://tecosaur.github.io/emacs-config/config.html
+;; https://learnxinyminutes.com/docs/zh-cn/elisp-cn/
+;; https://www.cheng92.com/emacs/doom-emacs-with-org/
+;; Tutorial: https://learnxinyminutes.com/docs/zh-cn/elisp-cn/
+;; http://smacs.github.io/elisp/
+;;
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "DeJaVu Sans Mono" :size 28 :weight 'regular))
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 16))
+;; (setq doom-font (font-spec :family "Droid Sans Mono" :size 20))
+;; doom-variable-pitch-font (font-spec :family "sans" :size 16))
+
+;;; Add to ~/.doom.d/config.el
+;; (setq doom-font (font-spec :family "Droid Sans Mono" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
+;;       doom-unicode-font (font-spec :family "Droid Sans Mono" :size 12)
+;;       doom-big-font (font-spec :family "Fira Mono" :size 19))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;; doom-one doom-dracula doom-vibrant 'doom-solarized-dark
+(setq doom-theme 'doom-one)
+
+(setq org-directory "~/org/")
+(setq org-superstar-headline-bullets-list '("☰" "✿" "☷" ))
+(setq org-startup-folded t)
+(setq org-startup-indented t)
+(setq org-ellipsis "▼")
+
+(setq make-backup-files nil)
+(setq company-idle-delay 0.1)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+;; (pushnew! initial-frame-alist '(width . 200) '(height . 55))
+;; (add-hook 'window-setup-hook #'toggle-frame-maximized)
+(add-hook 'window-setup-hook #'toggle-frame-fullscreen)
+;; (setq frame-resize-pixelwise t)
+
+;; (add-hook! '+doom-dashboard-functions :append
+;;   (insert "\n" (+doom-dashboard--center +doom-dashboard--width "Powered by Emacs!")))
+
+;;  home cursor
+;; ( setq-hook! '+doom-dashboard-mode-hook evil-normal-state-cursor (list nil))
+
+;; fast search use spc jj
+(setq avy-all-windows t)
+(map! :leader
+      :prefix "j"
+      :desc "avy-goto-char-timer" "j" #'avy-goto-char-timer)
+
+(setq doom-modeline-enable-word-count t)
+
+(setq company-idle-delay 0.1)
+
+;; Disable invasive lsp-mode features
+(setq lsp-ui-sideline-enable nil   ; not anymore useful than flycheck
+      lsp-ui-doc-enable nil        ; slow and redundant with K
+      lsp-enable-symbol-highlighting nil
+      +lsp-prompt-to-install-server 'quiet)
+
+;; Prevents some cases of Emacs flickering
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
+
+;; Focus new window after splitting
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
+
+;; use yasnipets
+(setq yas-triggers-in-field t)
+
+;; fast quit emacs
+(setq confirm-kill-emacs nil)
---
+++ doomemacs/.doom.d/init.el
@@ -0,0 +1,60 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; This file controls what Doom modules are enabled and what order they load
+;; in. Remember to run 'doom sync' after modifying it!
+
+;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
+;;      documentation. There you'll find a "Module Index" link where you'll find
+;;      a comprehensive list of Doom's modules and what flags they support.
+
+;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
+;;      'C-c c k' for non-vim users) to view its documentation. This works on
+;;      flags as well (those symbols that start with a plus).
+;;
+;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
+;;      directory (for easy access to its source code).
+
+(doom! :input
+
+       :completion
+       yasnippets
+       company          ; the ultimate code completion backend
+       ivy                 ; a search engine for love and life
+
+       :ui
+       doom              ; what makes DOOM look the way it does
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       indent-guides     ; highlighted indent columns
+       (modeline   +light)       ; snazzy, Atom-inspired modeline, plus API
+       neotree           ; a project drawer, like NERDTree for vim
+       tabs              ; a tab bar for Emacs
+       unicode           ; extended unicode support for various languages
+
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       (format +onsave)  ; automated prettiness
+       snippets          ; my elves. They type so I don't have to
+
+       :emacs
+       (dired +icons)            ; making dired pretty [functional]
+       (ibuffer +icons)        ; interactive buffer management
+
+       :checkers
+       syntax              ; tasing you for every semicolon you forget
+       grammar           ; tasing grammar mistake every you make
+
+       :tools
+       (eval +overlay)     ; run code, run (also, repls)
+       lookup              ; navigate your code and its documentation
+       lsp                 ; M-x vscode
+       rgb                 ;creating color strings
+
+       :lang
+       cc                ; C > C++ == 1
+       emacs-lisp          ; drown in parentheses
+       (org +pretty +present)
+
+       :config
+       literate
+       (default +bindings +smartparens))
