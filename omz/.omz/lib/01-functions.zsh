12dbb1ceedf1e428bdf32ea2e9af41e665fd78ad
oeyoews <jyao4783@gmail.com>
2022-01-23 17:11:04 +0800

    doc: update(automatically)

--- omz/.omz/lib/01-functions.zsh
+++ omz/.omz/lib/01-functions.zsh
@@ -256,22 +256,22 @@ function pym() {
   # or use npx http-server
 }
 
-# @nvm
-function onvm() {
-  export NVM=/usr/share/nvm
-  export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node
-  source $NVM/init-nvm.sh
-}
+# @nvm @deprecated
+#function onvm() {
+  #export NVM=/usr/share/nvm
+  #export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node
+  #source $NVM/init-nvm.sh
+#}
 
 # @proxy
-function onproxy1() {
-   export https_proxy=http://127.0.0.1:8889
-   export http_proxy=http://127.0.0.1:8889
-   export all_proxy=socks5://127.0.0.1:1089
-   # curl cip.cc 代理后 cip.cc 访问会很慢
-   # curl ip.sb
- }
-alias offproxy1="unset http_proxy https_proxy all_proxy && curl cip.cc"
+#function onproxy1() {
+   #export https_proxy=http://127.0.0.1:8889
+   #export http_proxy=http://127.0.0.1:8889
+   #export all_proxy=socks5://127.0.0.1:1089
+   ## curl cip.cc 代理后 cip.cc 访问会很慢
+   ## curl ip.sb
+ #}
+#alias offproxy1="unset http_proxy https_proxy all_proxy && curl cip.cc"
 
 
 # color
@@ -289,13 +289,13 @@ function nvm_prompt_info() {
 zmodload zsh/zprof
 
 # TMUX
-function tmuxa() {
-if which tmux >/dev/null 2>&1; then
-    #if not inside a tmux session, and if no session is started, start a new session
-    # ???
-    test -z "$TMUX" && { tmux attach || tmux new-session; }
-fi
-}
+#function tmuxa() {
+#if which tmux >/dev/null 2>&1; then
+    ##if not inside a tmux session, and if no session is started, start a new session
+    ## ???
+    #test -z "$TMUX" && { tmux attach || tmux new-session; }
+#fi
+#}
 
 # how to better these cd (all in one)
 function cdt() {
