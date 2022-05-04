b735ab80ade457742b29f7c9dfc77722bf82b6e2
oeyoews <jyao4783@gmail.com>
2022-05-04 14:33:46 +0800

    🎣 This is an automatic push by @oeyoews

--- omz/.omz/lib/alias.zsh
+++ omz/.omz/lib/alias.zsh
@@ -95,7 +95,7 @@ alias gitc1="git clone --depth 1"
 
 #alias gitp="git push 🐬"
 alias gitu='git add . && git commit && git push'
-alias gitua='git add . && git commit -m "🎣 This is an automatic push" && git push'
+alias gitua='git add . && git commit -m "🎣 This is an automatic push by @oeyoews" && git push'
 alias gituad='git add . && git commit -m "🔵 This is an automatic push" && git push'
 alias gituai='git commit -m "🧋 This is an automatic push for img" && git push'
 
