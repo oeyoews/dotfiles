c1052a3f0e7111c08f850fe125e0c0ffefd97e3f
oeyoews <jyao4783@gmail.com>
2022-06-03 17:07:04 +0800

    🍰 This is an automatic push

--- dot_config/zsh/themes/p10k/p10k-vanilla.zsh
+++ dot_config/zsh/themes/p10k/p10k-vanilla.zsh
@@ -109,7 +109,7 @@
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=44
   # Custom icon.    
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='  '
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=' '
   #"\U1F608"
   #typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=""
 
@@ -119,11 +119,11 @@
   # Red prompt symbol if the last command failed.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=196
   # Default prompt symbol. ❯
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION=' '
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION=''
   # Prompt symbol in command vi mode. ❮
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION=' '
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION=''
   # Prompt symbol in visual vi mode.V
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION=' V'
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION='V'
   # Prompt symbol in overwrite vi mode.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIOWR_CONTENT_EXPANSION='▶'
   typeset -g POWERLEVEL9K_PROMPT_CHAR_OVERWRITE_STATE=true
