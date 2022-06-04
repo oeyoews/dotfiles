992bcf2d5b6e83524f95f298fa5df1166036a697
oeyoews <jyao4783@gmail.com>
2022-06-04 09:00:31 +0800

    🍰 This is an automatic push

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+UserLocation=0
+read -r -p "Are you sure to install dotfiles? 🍻 [y|N] " response
+if [[ $response =~ (y|yes|Y) ]];then
+  UserLocation=1
+  print("Installing ... 💉")
+fi
+
 # TODO: add remove old or bk odl chezmoi and acquire user if to bk or delete
 chezmoi init  --depth 1 https://gitlab.com/oeyoews/dotfiles.git
 chezmoi apply
 #chezmoi update -R
+
+#main() {
+
+#}
+
+#main
