f2d067e6f1157551ff760967eba00a6cd5cd1dd3
oeyoews <jyao4783@gmail.com>
2022-06-18 00:12:35 +0800

    fix: replace beer to guitar

--- dot_config/ohmyzsh/lib/alias.zsh
+++ dot_config/ohmyzsh/lib/alias.zsh
@@ -95,7 +95,7 @@ alias gitc1="git clone --depth 1"
 alias gitpp="git pull --rebase=true && git push"
 
 alias gitu='git add . && git commit && git push'
-alias gitua='git add . && git commit -m ":beer: This is an automatic push" && git push'
+alias gitua='git add . && git commit -m "🎸 This is an automatic push" && git push'
 alias gituai='git commit -m "🧋 This is an automatic push for img" && git push'
 
 #remove tag
