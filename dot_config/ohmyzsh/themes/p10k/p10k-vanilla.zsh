68e7b5c4a8d3895d90a588ae4f7b09207b64be39
oeyoews <jyao4783@gmail.com>
2022-06-21 21:30:59 +0800

    🦅 This is an automatic push

--- dot_config/ohmyzsh/themes/p10k/p10k-vanilla.zsh
+++ dot_config/ohmyzsh/themes/p10k/p10k-vanilla.zsh
@@ -109,7 +109,7 @@
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=44
   # Custom icon.       🐦️ 💡 🇻 📣
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=' '
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='勇' # 
   #"\U1F608"
   #typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=""
 
