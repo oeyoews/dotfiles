0eb4fb07b7edabfd4a2e8645dca2e9916dea0329
oeyoews <jyao4783@gmail.com>
2022-06-29 18:03:46 +0800

    🧋 This is an automatic push

--- dot_config/ohmyzsh/lib/01-functions.zsh
+++ dot_config/ohmyzsh/lib/01-functions.zsh
@@ -512,6 +512,17 @@ function cdr() {
   #wd $1 | clipcopy && wd $1
 #}
 
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
 setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
