555d128cd78937cdc7069ddab2741902c24deb4a
oeyoews <jyao4783@gmail.com>
2022-08-10 13:21:05 +0800

    Automatic commit

--- dot_config/ohmyzsh/lib/alias.zsh
+++ dot_config/ohmyzsh/lib/alias.zsh
@@ -1,6 +1,7 @@
 # note this will cause $path load twice extra
 # bug: exe it twice
 #alias soz='source $HOME/.zshrc && exec zsh'
+alias tlmgr = "/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode"
 alias fs="fc-list :family style"
 alias pasm="sudo pacman-mirrors -c China -m rank -i -aS unstable"
 alias cdd="cd ~/.local/share/chezmoi"
