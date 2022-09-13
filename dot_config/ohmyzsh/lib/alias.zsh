6bb9a9af159297a9d36ec34c892e8e2fe3abee82
oeyoews <jyao4783@gmail.com>
2022-09-13 10:40:46 +0800

    Automatic commit

--- dot_config/ohmyzsh/lib/alias.zsh
+++ dot_config/ohmyzsh/lib/alias.zsh
@@ -3,6 +3,7 @@
 #alias soz='source $HOME/.zshrc && exec zsh'
 # have output bug
 # alias tlmgr = "/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode"
+alias gd="github-desktop"
 alias neovide="neovide --multigrid"
 alias za="zathura"
 alias fs="fc-list :family style"
